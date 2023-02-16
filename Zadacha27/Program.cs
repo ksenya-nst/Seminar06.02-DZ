627bb23fc9b1a14e633f0979b5bf7c6b907740aa
Kseniya <ksenya-nst@mail.ru>
2023-02-16 20:29:16 +0500

    add Zadacha27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int num = GetNumber("Введите число: ");
+int len = NumberLen(num);
+SumNumbers(num, len);
+
+int GetNumber(string text)
+{
+ System.Console.WriteLine(text);
+ return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumberLen(int a)
+{
+ int index = 0;
+ while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    System.Console.WriteLine($"Сумма цифр числа {num} = {sum}");
+}
